74955b6041f77bdbd0fca850774e1ba9dd33221b
chumples <84584789+chumples@users.noreply.github.com>
2021-07-10 18:51:56 -0700

    added button functionality

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
-    this.pages = pages
+    this.pages = pages,
+    this.read = read
     this.addBookToLibrary()
     // myLibrary.push(this)
     // this.addBookToLibrary()
@@ -13,12 +14,15 @@ Book.prototype.addBookToLibrary = function() {
     myLibrary.push(this)
 }
 
-
-
-function displayBooks() {
+function displayBooks(Book) {
 
 }
 
-const Tao = new Book('Tao Te Ching', 'Laozi', 88);
-const Gatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180);
-const Catcher = new Book('The Catcher in the Rye', 'J.D. Salinger', 277);
+const newBook = document.getElementById('new');
+newBook.onclick = function() {
+    alert('Hi!')
+}
+
+const Tao = new Book('Tao Te Ching', 'Laozi', 88, true);
+const Gatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
+const Catcher = new Book('The Catcher in the Rye', 'J.D. Salinger', 277, true);
