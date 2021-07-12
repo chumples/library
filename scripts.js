35cb41e713d407572794df036b67298729123651
chumples <84584789+chumples@users.noreply.github.com>
2021-07-12 13:13:33 -0700

    centered things

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 let myLibrary = [];
+let myLibraryLength = myLibrary.length;
+for (let i = 0; i < myLibraryLength.length; i++) {
+    console.log(myLibrary[i])
+}
 
 function Book(title, author, pages, read) {
     this.title = title,
@@ -19,10 +23,14 @@ function displayBooks(Book) {
 }
 
 const newBook = document.getElementById('new');
-newBook.onclick = function() {
-    alert('Hi!')
-}
+// newBook.onclick = function() {
+//     alert('Hi!')
+// }
+newBook.addEventListener('click', function() {
+
+});
 
 const Tao = new Book('Tao Te Ching', 'Laozi', 88, true);
 const Gatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
 const Catcher = new Book('The Catcher in the Rye', 'J.D. Salinger', 277, true);
+
