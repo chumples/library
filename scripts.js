8e8790e1ed540544684a27e498310f956f3d77be
chumples <84584789+chumples@users.noreply.github.com>
2021-07-12 18:31:09 -0700

    attempt to append array

--- scripts.js
+++ scripts.js
@@ -34,16 +34,35 @@ const Tao = new Book('Tao Te Ching', 'Laozi', 88, true);
 const Gatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
 const Catcher = new Book('The Catcher in the Rye', 'J.D. Salinger', 277, true);
 
+// const tag = document.createElement('p')
+// // tag.appendChild(Tao);
+// const text = document.createTextNode('TEST');
+// tag.appendChild(text);
+// // tag.appendChild(myLibrary[0]);
+// const element = document.getElementById('bookList');
+// element.appendChild(tag);
+
+// const list = document.getElementById('bookList');
+// const cellTag = list.appendChild(document.createElement('td'));
+// cellTag.id = 'level1';
+
+// for (var i = 0; i < myLibrary.length; i++) {
+//     cellTag.appendChild(document.createTextNode(myLibrary[i]));
+//     cellTag.appendChild(document.createElement('br'));
+// }
+
+document.getElementById('bookList').innerHTML = myLibrary.values(Book);
+
+
+
 function newButton() {
     document.getElementById('myForm').style.display = 'block';
 }
 
-
-
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+    document.getElementById('myForm').style.display = 'block';
   }
   
   function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+    document.getElementById('myForm').style.display = 'none';
   }
